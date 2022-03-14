05c9722c6dfb0d9f7441952549a987a8ee2552a7
Vaughn Kottler <vaughnkottler@gmail.com>
2022-03-14 04:04:09 -0700

    Update package build.

---
+++ svgen/__main__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=1.13.5
+# hash=c899afd4b2308889f0dc1260eede4d46
+# =====================================
+"""
+svgen - Package's default entry-point.
+"""
+
+# built-in
+import sys
+
+# internal
+from svgen.entry import main
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -3,9 +3,12 @@ svgen - Test the program's entry-point.
 """
 
 # built-in
+from subprocess import check_output
+from sys import executable
 from unittest.mock import patch
 
 # module under test
+from svgen import PKG_NAME
 from svgen.entry import main as svgen_main
 
 # internal
@@ -23,3 +26,9 @@ def test_entry_basic():
 
     with base_args(include_config=False) as args:
         assert svgen_main(args) == 0
+
+
+def test_package_entry():
+    """Test the command-line entry through the 'python -m' invocation."""
+
+    check_output([executable, "-m", PKG_NAME, "-h"])
