14e66f13ad35e99e4dc10922039fa02b4c037aa6
Vaughn Kottler <vaughnkottler@gmail.com>
2022-10-07 00:29:27 -0500

    Try to improve coverage again

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -4,8 +4,10 @@ svgen - Test the program's entry-point.
 
 # built-in
 from asyncio import run
+from contextlib import suppress
 from subprocess import check_output
 from sys import executable
+from typing import Coroutine
 from unittest.mock import patch
 
 # module under test
@@ -19,18 +21,29 @@ from tests.resources import base_args
 def test_entry_basic():
     """Test basic argument parsing."""
 
-    # Patch 'asyncio.run' so that icon generation runs.
-    with patch("svgen.generation.images.run", new=run) as mock:
-        mock.return_value = True
-
-        with base_args() as args:
-            assert svgen_main(args) == 0
-
-            with patch("svgen.entry.entry", side_effect=SystemExit(1)):
-                assert svgen_main(args) != 0
+    with base_args() as args:
+        with patch("svgen.entry.entry", side_effect=SystemExit(1)):
+            assert svgen_main(args) != 0
 
     with base_args(include_config=False) as args:
         assert svgen_main(args) == 0
+
+
+def test_entry_bypass_generation():
+    """
+    Test image generation logic even if 'inkscape' is not found on the system.
+    """
+
+    def run_patch(awaitable: Coroutine) -> bool:
+        """Run the coroutine but always return True."""
+        with suppress(FileNotFoundError):
+            run(awaitable)
+        return True
+
+    # Patch 'asyncio.run' so that icon generation runs.
+    with patch("svgen.generation.images.run", new=run_patch):
+        with base_args() as args:
+            assert svgen_main(args) == 0
 
 
 def test_package_entry():
