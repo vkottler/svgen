0865dda179306a81409ff3c1337506b86ba69359
Vaughn Kottler <vaughnkottler@gmail.com>
2022-10-06 23:59:47 -0500

    Attempt to improve coverage

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -3,6 +3,7 @@ svgen - Test the program's entry-point.
 """
 
 # built-in
+from asyncio import run
 from subprocess import check_output
 from sys import executable
 from unittest.mock import patch
@@ -18,11 +19,15 @@ from tests.resources import base_args
 def test_entry_basic():
     """Test basic argument parsing."""
 
-    with base_args() as args:
-        assert svgen_main(args) == 0
+    # Patch 'asyncio.run' so that icon generation runs.
+    with patch("svgen.generation.images.run", new=run) as mock:
+        mock.return_value = True
 
-        with patch("svgen.entry.entry", side_effect=SystemExit(1)):
-            assert svgen_main(args) != 0
+        with base_args() as args:
+            assert svgen_main(args) == 0
+
+            with patch("svgen.entry.entry", side_effect=SystemExit(1)):
+                assert svgen_main(args) != 0
 
     with base_args(include_config=False) as args:
         assert svgen_main(args) == 0
