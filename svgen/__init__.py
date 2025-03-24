f1cfa40fb4d0311af6b2a0d2d6ba46e5a9ed3817
Vaughn Kottler <vaughnkottler@gmail.com>
2025-03-24 01:49:10 -0500

    0.7.5 - Add bootstrap theme

--- svgen/__init__.py
+++ svgen/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.4
-# hash=c4e94d744b865ee8bfb68a6dcb95bc29
+# hash=dc76f2211c86940a5a0923d60d03feae
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A tool for working with scalable vector graphics."
 PKG_NAME = "svgen"
-VERSION = "0.7.4"
+VERSION = "0.7.5"
